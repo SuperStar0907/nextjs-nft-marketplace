bc7924289a92c307ebde1daa08cdf038ad91c941
Tirth Motka <tirthmotka@gmail.com>
2023-03-10 19:39:52 +0530

    Deployed on Matic testnet

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 
-  export const marketplaceAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+  export const marketplaceAddress = "0xF8f9601b7fDea3daD13Bdd3fAed5303297a1DB8F"
   
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,17 @@
 /* hardhat.config.js */
 require("@nomiclabs/hardhat-waffle")
-
+console.log(process.env.privateKey)
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      chainId: 1337
+      chainId: 5
     },
 //  unused configuration commented out for now
-//  mumbai: {
-//    url: "https://rpc-mumbai.maticvigil.com",
-//    accounts: [process.env.privateKey]
-//  }
+ mumbai: {
+   url: "https://rpc-mumbai.maticvigil.com/v1/f25f5f243541031184d4dcbdabf5da417594a7a3",
+   accounts: ["0x689af8efa8c651a91ad287602527f3af2fe9f6501a7ac4b061667b5a93e037fd"]
+ }
   },
   solidity: {
     version: "0.8.4",
